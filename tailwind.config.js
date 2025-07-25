bd7cbebaadabd9ac1fe705853a62aa1114bc7825
farrelac <carasiola46@gmail.com>
2025-07-25 18:59:04 +0700

    edit more4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-import forms from "@tailwindcss/forms";
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    // --- TAMBAHKAN BARIS INI UNTUK MEMATIKAN DARK MODE ---
-    darkMode: false,
-
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-        },
+  darkMode: 'class', // Ubah dari `false` ke 'class' atau 'media' jika butuh dark mode
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue", // Tambahkan jika menggunakan Vue
+    "./storage/framework/views/*.php",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
     },
-
-    plugins: [forms],
-};
+  },
+  plugins: [
+    forms(),
+    // Tambahkan plugin lain jika diperlukan
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/aspect-ratio'),
+  ],
+  corePlugins: {
+    preflight: true, // Pastikan true untuk reset CSS
+  }
+}
